6a692d414f03cfaa629a24cf8fe6676c64caf6f4
dudu <1344812937@qq.com>
2025-08-17 01:02:17 +0800

    feat(定时任务)：定时任务管理优化，支持配置任务初始化调用功能

--- internal/tasks/task.go
+++ internal/tasks/task.go
@@ -8,23 +8,60 @@ import (
 )
 
 type CronTask struct {
-	cron *cron.Cron
+	cron     *cron.Cron
+	TaskItem []*CornTaskItem
+}
+
+type CornTaskItem struct {
+	// 任务名称
+	Name string
+	// corn定时任务spec
+	Spec string
+	// 执行方法
+	Handler func()
+	// 是否需要首次执行
+	FirstExecuted bool
 }
 
 // NewCronTask 注册当前定时任务
 func NewCronTask(mavenService *service.MavenService) *CronTask {
-	until.Log.Info("定时任务注册")
 	res := &CronTask{cron: cron.New()}
-	_, err := res.cron.AddFunc("*/5 * * * *", func() {
-		until.Log.Info("扫描本地目录任务开始")
-		mavenService.GetLocalMavenRepo()
-		until.Log.Info("扫描本地目录任务结束")
+	res.RegisterTask(&CornTaskItem{"本地资源扫描", "*/5 * * * *", mavenService.GetLocalMavenRepo, true})
+	defer res.firstExecute()
+	return res
+}
+
+// 首要任务执行器
+func (t *CronTask) firstExecute() {
+	item := t.TaskItem
+	if item != nil {
+		for _, taskItem := range item {
+			if taskItem.FirstExecuted {
+				t.execute(taskItem)
+			}
+		}
+	}
+}
+
+// RegisterTask 任务注册
+func (t *CronTask) RegisterTask(taskItem *CornTaskItem) {
+	until.Log.Info("注册任务开始：" + taskItem.Name + " spec:" + taskItem.Spec)
+	_, err := t.cron.AddFunc(taskItem.Spec, func() {
+		t.execute(taskItem)
 	})
 	if err != nil {
-		panic(err)
+		until.Log.Info("任务注册失败：" + taskItem.Name)
+		return
 	}
-	until.Log.Info("定时任务注册成功")
-	return res
+	t.TaskItem = append(t.TaskItem, taskItem)
+	until.Log.Info("任务注册成功：" + taskItem.Name)
+}
+
+// 定时任务实际执行器
+func (t *CronTask) execute(taskItem *CornTaskItem) {
+	until.Log.Info("任务调度执行开始：" + taskItem.Name)
+	taskItem.Handler()
+	until.Log.Info("任务调度执行结束：" + taskItem.Name)
 }
 
 func (t *CronTask) Start() {
