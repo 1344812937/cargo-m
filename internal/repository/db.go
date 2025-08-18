9858d8eb346a6cd0560ecb31a4e942ce3342c92d
张廷东 <gyrjgy6@supcon.com>
2025-08-18 11:22:36 +0800

    fix: 去除sqllite CGO依赖，linux兼容问题解决

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -3,16 +3,16 @@ package repository
 import (
 	"cargo-m/internal/model"
 
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
 var Db gorm.DB
 
 func init() {
-	db, err := gorm.Open(sqlite.Open(".\\data.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("failed to connect database" + err.Error())
 	}
 	db.AutoMigrate(&model.MavenArtifactModel{})
 	Db = *db
